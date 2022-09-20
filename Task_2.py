d3206f1d9c74b0ed48826ff69e447d15b349ec64
My Name <Me@gmail.com>
2022-09-20 21:06:58 +0400

    1 commit

--- Task_2.py
+++ Task_2.py
@@ -1,1 +1,25 @@
 # Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+с = int(input('Введите третье число: '))
+d = int(input('Введите четвёртое число: '))
+e = int(input('Введите пятое число: '))
+max = 0
+if a > b:
+    max = a
+elif b > a:
+    max = b
+elif c > max:
+    max = c
+else:
+    max = max
+if d > max:
+    max = d
+else:
+    max = max
+if e > max:
+    max = d
+else:
+    max = max
+print(max)
